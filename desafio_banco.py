56d63331f44defde41293eb38bb663d830157169
Kauan Henrique <kauanbrpe.dev@hotmail.com>
2025-08-05 12:35:14 -0300

    first commit

---
+++ desafio_banco.py
@@ -0,0 +1,67 @@
+print("Banco de Desenvolvimento do Estado de Pernambuco - BANDEPE")
+print("    GOVERNO DO ESTADO DE PERNAMBUCO - ESTADO DE MUDANÇA   ")
+print("==========================================================")
+
+menu = """
+
+[D] Depositar
+[S] Sacar
+[E] Extrato
+[Q] Sair
+
+"""
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+while True:
+    opcao = input(menu)
+
+    if opcao == "D":
+        valor = float(input("Informe o valor do depósito: "))
+
+        if valor > 0:
+            saldo += valor
+            extrato += f"Deposíto: R$ {valor:.2f}\n"
+
+        else:
+            print("Operação falhou! O valor informado é inválido!")
+
+    if opcao == "S":
+        valor = float(input("Informe o valor do saque: "))
+
+        excedeu_saldo = valor > saldo
+
+        excedeu_limite = valor > limite
+
+        excedeu_saques = numero_saques >= LIMITE_SAQUES
+
+        if excedeu_saldo:
+            print("Operação falhou! Você não tem saldo suficiente.")
+        elif excedeu_limite:
+            print("Operação falhou! O valor do saque excedeu o limite")
+        elif excedeu_saques:
+            print("Operação falhou! Número máximo de saques excedido.")
+
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque: R$ {valor:.2f}"
+            numero_saques += 1
+
+    if opcao == "E":
+        print("\n=============== EXTRATO ==============")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo: R$ {valor:.2f}")
+        print("========================================")
+
+    elif opcao == "q":
+        print("      Obrigado por usar o sistema do BANDEPE!      ")
+        print("GOVERNO DO ESTADO DE PERNAMBUCO - ESTADO DE MUDANÇA")
+        print("===================================================")
+        break
+
+    else:
+        print("Operação inválida, por favor selecione novamente a operação desejada.")
